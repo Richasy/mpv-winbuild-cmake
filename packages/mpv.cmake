6ebee5aa3a04c9c464afa51357306b4034db674b
Richasy <Thansy@foxmail.com>
2024-09-06 21:25:15 +0800

    Update mpv.cmake

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -23,7 +23,7 @@ ExternalProject_Add(mpv
         vapoursynth
         libsdl2
     GIT_REPOSITORY https://github.com/Richasy/mpv.git
-    GIT_TAG origin/d3d11
+    GIT_TAG origin/uwp
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
